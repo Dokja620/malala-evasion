772392fdb1303e8389e07fb08296814dbedbe4f3
HomeJa <revanamike620@gmail.com>
2024-03-09 16:32:08 +0300

    Fully responsive: - Intro section - Navbar - Footer

---
+++ assets/scripts/site.js
@@ -0,0 +1,31 @@
+// Nav bar
+const menuBurger = document.querySelector('.menu-burger');
+const navLinks = document.querySelector('.nav-links');
+const bar = document.querySelector('.bar-f');
+const bars = document.querySelector('.bar-s');
+
+menuBurger.addEventListener('click', () => {
+    toggleMenu();
+});
+
+// Function to toggle the menu
+function toggleMenu() {
+    navLinks.classList.toggle('mobile-menu');
+    menuBurger.classList.toggle('shine');
+    bar.classList.toggle('shrink');
+    bars.classList.toggle('shrinks');
+
+    // Toggle menu when clicking outside navLinks
+    document.addEventListener('click', (event) => {
+        const isClickInsideNavLinks = navLinks.contains(event.target);
+        const isClickInsideMenuBurger = menuBurger.contains(event.target);
+
+        if (!isClickInsideNavLinks && !isClickInsideMenuBurger) {
+            navLinks.classList.remove('mobile-menu');
+            menuBurger.classList.remove('shine');
+            bar.classList.remove('shrink');
+            bars.classList.remove('shrinks');
+        }
+    });
+}
+s
